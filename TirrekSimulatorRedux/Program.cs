8aef879a5c3aab59b53f03c02a24016cd86b2254
ElectircJager <gozyuksel@mail.ru>
2021-07-02 00:03:08 +0300

    Initial build & splashscreen

---
+++ TirrekSimulatorRedux/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TirrekSimulatorRedux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SplashScreen();
+            Console.WriteLine("Hello World!");
+        }
+        
+        static void SplashScreen()
+        {
+            Console.WriteLine("Tirrek Simulator Redux\nElectricJager 2021\n");
+        }
+    }
+}
